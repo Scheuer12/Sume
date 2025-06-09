5def387498d5926521a45786a206f3d03f19ff50
Scheuer12 <carlos@epyatis.com.br>
2025-06-09 10:53:51 -0300

    API security updated

--- backend/api/eye_integration.py
+++ backend/api/eye_integration.py
@@ -3,43 +3,58 @@ import os
 from dotenv import load_dotenv
 
 
-
-load_dotenv()
-
-ACCESS_KEY = os.getenv("EYEMOBILE_ACCESS_KEY")
-SECRET_KEY = os.getenv("EYEMOBILE_SECRET_KEY")
-
-headers = {
-    "X-EYEMOBILE-ACCESS-KEY": ACCESS_KEY,
-    "X-EYEMOBILE-SECRET-KEY": SECRET_KEY,
-    "Content-Type": "application/json"
-}
+class eyeapi:
 
 
-
-url = "https://api.eyemobile.com.br/v1/synchronizations/transactions/lock"
-response = requests.get(url, headers=headers)
-
-if response.ok:
-    data = response.json()
-    payload = []
-
-    for item in data.get("data"):
-        payload.append({
-            response("data"("id")),
-            True,
-            "Venda registrada com sucesso",
-            None
-        })
     
+    def __init__(self):
 
-    res = requests.post(
-        "https://api.eyemobile.com.br/synchronizations/transactions/confirm",
-        headers=headers,
-        json=payload
-    )
+        load_dotenv()
 
-    if res.ok:
-        print("Sincronização confirmada com sucesso.")
-    else:
-        print(f"Erro ao confirmar: {res.status_code} - {res.text}")
+        self.ACCESS_KEY = os.getenv("EYEMOBILE_ACCESS_KEY")
+        self.SECRET_KEY = os.getenv("EYEMOBILE_SECRET_KEY")
+
+        self.headers = {
+            "X-EYEMOBILE-ACCESS-KEY": self.ACCESS_KEY,
+            "X-EYEMOBILE-SECRET-KEY": self.SECRET_KEY,
+            "Content-Type": "application/json"
+        }
+
+        self.response = None
+        self.url = ""
+
+
+
+
+    def request(self):
+
+        self.url = "https://api.eyemobile.com.br/v1/synchronizations/transactions/lock"
+        self.response = requests.get(self.url, headers=self.headers)
+
+        if self.response.ok:
+            data = self.response.json()
+            payload = []
+
+            for item in data.get("data"):
+                payload.append({
+                    item["id"],
+                    True,
+                    "Venda registrada com sucesso",
+                    None
+                })
+            
+
+            res = requests.post(
+                "https://api.eyemobile.com.br/synchronizations/transactions/confirm",
+                headers=self.headers,
+                json=payload
+            )
+
+            if res.ok:
+                print("Sincronização confirmada com sucesso.")
+            else:
+                print(f"Erro ao confirmar: {res.status_code} - {res.text}")
+
+        else:
+
+            print("Falha na importação. Tente novamente.")
